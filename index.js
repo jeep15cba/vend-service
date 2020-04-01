eb2ec79f52dfd53ecab26e7de2bb3ab507a49728
Adam Young <adam@mydrones.com.au>
2020-04-02 09:45:44 +1000

    google oauth and mongodb added

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const AuthTokenStrategy = require('passport-auth-token').Strategy;
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app);
+
+
+
+//passport.use('authtoken', new AuthTokenStrategy(
+//  function(token, done) {
+//    AccessToken.findOne({
+//      id: token
+//    }, function(error, accessToken) {
+//      if (error) {
+//        return done(error);
+//      }
+//
+//      if (accessToken) {
+//        if (!token.isValid(accessToken)) {
+//          return done(null, false);
+//        }
+//
+//        User.findOne({
+//          id: accessToken.userId
+//        }, function(error, user) {
+//          if (error) {
+//            return done(error);
+//          }
+//
+//          if (!user) {
+//            return done(null, false);
+//          }
+//
+//          return done(null, user);
+//        });
+//      } else {
+//        return done(null);
+//      }
+//    });
+//  }
+//));
+//
+//app.get('/auth/vend',
+//  passport.authenticate(
+//    'authtoken',
+//    {
+//      session: false,
+//      optional: false
+//    }
+//  ),
+//  function(req, res) {
+//    res.redirect('/auth/vend/callback');
+//  }
+//);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
